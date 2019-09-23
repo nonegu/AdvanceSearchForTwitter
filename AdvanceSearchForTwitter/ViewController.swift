6a4772957ebd70a5fa03c720ac78412b30cd2394
Ender Güzel <enderguzel@gmail.com>
2019-09-23 15:06:40 +0300

    feat: Create SignInViewController UI

--- AdvanceSearchForTwitter/ViewController.swift
+++ AdvanceSearchForTwitter/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var signInButton: UIButton!
+    
+    // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+        signInButton.layer.cornerRadius = 5
+    }
+    
+    // MARK: Actions
+    @IBAction func signInPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
