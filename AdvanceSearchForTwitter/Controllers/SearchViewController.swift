20ec6ebd7fa6f8650f9b0418fbb5fd51b6475019
Ender Güzel <enderguzel@gmail.com>
2019-09-25 12:31:16 +0300

    feat: Create SearchViewController UI

---
+++ AdvanceSearchForTwitter/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  AdvanceSearchForTwitter
+//
+//  Created by Ender Güzel on 25.09.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
