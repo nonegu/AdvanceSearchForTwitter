136d8d0407f9da7d01ba69b3907edd60c4d33999
Ender Güzel <enderguzel@gmail.com>
2019-09-27 14:57:57 +0300

    fix: Refactor the collectionViewFlowLayout for identical cell sizes
    This is a temporary fix, the code will be updated for automatic sizes for each cell's content

--- AdvanceSearchForTwitter/Controllers/HomeViewController.swift
+++ AdvanceSearchForTwitter/Controllers/HomeViewController.swift
@@ -19,18 +19,24 @@ class HomeViewController: UIViewController {
     // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
 //        TwitterAPI.get(url: URL(string: "https://api.twitter.com/1.1/search/tweets.json?q=from%3Atwitterdev&result_type=mixed&count=5&tweet_mode=extended")!, completion: handleSearchResults(results:error:))
-        TwitterAPI.getTimeline(url: URL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json?tweet_mode=extended")!, completion: handleSearchResults(results:error:))
+        TwitterAPI.getTimeline(url: URL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json?count=40&tweet_mode=extended")!, completion: handleSearchResults(results:error:))
     }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = false
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+    
     func handleSearchResults(results: [Tweet], error: Error?) {
         if error == nil {
             tweets = results
-            print(tweets)
             DispatchQueue.main.async {
                 self.collectionView.reloadData()
             }
@@ -58,6 +64,10 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
         cell.profileImage.kf.setImage(with: URL(string: url))
         cell.layer.cornerRadius = 5
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width - 20, height: 180)
     }
     
     
